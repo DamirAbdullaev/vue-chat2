6969ce215fab082efe3a931000a8e8a28d782aab
Damir Abdullaev <abdullaevdamir34@gmail.com>
2024-07-16 16:48:57 +0300

    Создал проект на Vue
    *Устанновил все нужные зависимости
    *Подготовил проект к разработки

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Alex />
+  </div>
+</template>
+
+<script>
+import Alex from './components/Alex.vue'
+  export default {
+  components: { Alex },
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
