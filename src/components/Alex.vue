19d15d7b3757714f76ae4e123d2b99cfee3f64cf
Damir Abdullaev <abdullaevdamir34@gmail.com>
2024-07-17 21:44:22 +0300

    Создал компанент Alex.vue
    *Создал и оформил телефон Алекса
    *Немного проботал над методом для textarea
    там в принцепе всё понятно
    *Сделал все основные стили кроме окошка для
    отправки фото

--- src/components/Alex.vue
+++ src/components/Alex.vue
@@ -1,15 +1,66 @@
 <template>
-    <div>
-        <h1>ALEX</h1>
+    <div class="phone">
+      <div class="phone__header">
+        <img src="@/assets/img/alex.svg" alt="alex">
+        <div class="phone__header_info">
+          <p class="phone__header_info-name">Александр</p>
+          <span class="phone__header_info-status">Онлайн</span>
+        </div>
+      </div>
+  
+      <div class="phone__main">
+        <div class="phone__main_sms">
+          <p class="phone__main_sms-text">
+            <img src="@/assets/img/test_img.jpg" alt="test_img">
+            Привет. Как дела? Где ты изучаешь программирование?</p>
+          <span class="phone__main_sms-time">12:21</span>
+        </div>
+        <div class="phone__main_sms active">
+          <p class="phone__main_sms-text">
+            <img src="@/assets/img/test_img.jpg" alt="test_img">
+            Привет. Нормально. Как у тебя дела? Я учусь в учебном центре PROWEB</p>
+          <span class="phone__main_sms-time">12:21</span>
+        </div>
+      </div>
+  
+      <div class="phone__footer">
+        <form class="phone__footer_form">
+          <textarea ref="messageTextarea" v-model="message" placeholder="Написать сообщение..." :style="{ height: textareaHeight }"></textarea>
+          <button v-if="message"><img src="@/assets/img/send.svg" alt="send"></button>
+          <button v-else><img src="@/assets/img/photoaparat.svg" alt="photoaparat"></button>
+        </form>
+      </div>
     </div>
-</template>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        message: '',
+        textareaHeight: '56px', 
+        maxTextareaHeight: 150,
+      };
+    },
+    
+    methods: {
+        updateTextareaHeight() {
+            const textarea = this.$refs.messageTextarea;
+            textarea.style.height = '56px';
+            textarea.style.height = Math.min(textarea.scrollHeight, this.maxTextareaHeight) + 'px';
+            this.textareaHeight = textarea.style.height;
+        }
+    },
 
-<script>
-    export default {
-        
+    watch: {
+      message() {
+        this.$nextTick(this.updateTextareaHeight);
+      }
     }
-</script>
+  };
 
-<style lang="scss" scoped>
-
-</style>
+  </script>
+  
+  <style scoped>
+  </style>
+  
