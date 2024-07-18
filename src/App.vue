ddce7671bd0eff07efb702be50e97164dff133b4
Damir Abdullaev <abdullaevdamir34@gmail.com>
2024-07-18 12:22:36 +0300

    Создал modal.vue для img и добавил стили

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,33 @@
 <template>
   <div class="body">
-    <Alex :basees="basees"/>
-    <Eva :basees="basees"/>
+    <Alex :basees="basees" />
+    <Eva :basees="basees" />
+    <Modal />
   </div>
 </template>
 
 <script>
-import Alex from './components/Alex.vue'
-import Eva from './components/Eva.vue'
-  export default {
-  components: { Alex, Eva },
+import Alex from './components/Alex.vue';
+import Eva from './components/Eva.vue';
+import Modal from './components/Modal.vue';
+export default {
+  components: { Alex, Eva, Modal },
 
 
-    data() {
-      return {
-        basees: [
-          {name: 'Ева', url: '', text: 'Привет. Как дела? Где ты изучаешь программирование?', time: '13:23'},
-          {name: 'Александр', url: '', text: 'Привет. Нормально. Как у тебя дела? Я учусь в учебном центре PROWEB', time: '13:23'},
-          ],  
-      }
-    },
+  data() {
+    return {
+      basees: [
+        { name: 'Ева', url: '', text: 'Привет. Как дела? Где ты изучаешь программирование?', time: '13:23' },
+        { name: 'Александр', url: '', text: 'Привет. Нормально. Как у тебя дела? Я учусь в учебном центре PROWEB', time: '13:23' },
+      ],
+    }
+  },
 
-    methods: {
-      
-    },
-    
-  }
+  methods: {
+
+  },
+
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="modal">
+        <div class="modal__block">
+            <h2 class="modal__block-title">Отправить картинку</h2>
+            
+            <div class="modal__block_inputs">
+                <label>
+                    <span>URL</span>
+                    <input placeholder="URL" type="text">
+                </label>
+                <label>
+                    <span>Комментарий</span>
+                    <input placeholder="Комментарий" type="text">
+                </label>
+            </div>
+
+            <div class="modal__block_btn">
+                <button class="cancel">Отмена</button>
+                <button class="send">ОТПРАВИТЬ</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
